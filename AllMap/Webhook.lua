28d0b56cbb154bf43fb72391e123564cd9ca259b
Toonza555 <97265807+Toonza555@users.noreply.github.com>
2024-08-03 11:28:13 +0700

    Update Webhook.lua

--- AllMap/Webhook.lua
+++ AllMap/Webhook.lua
@@ -37,7 +37,7 @@ local webhook1 = request({
                     ["url"] = "https://cdn.discordapp.com/attachments/1012194764063514699/1250175514816282704/b9018579aebd5c161b0eac79ca04f17a.gif?ex=6669fc2d&is=6668aaad&hm=f39f28fa23a688313244381616c411979d0947cee220fe5b40e69723bc8e29a9&"
                 },
                 ["thumbnail"] = {
-                    ["url"] = "https://cdn.discordapp.com/attachments/1012194764063514699/1239910670464716820/Logo-B1.webp?ex=6661a54c&is=666053cc&hm=d7e309b3628e3ded2da9128a34a33453ef6f4e418b32044d23973c8b139c94a5&"
+                    ["url"] = "https://media.discordapp.net/attachments/1017383423918215229/1267360241381671035/standard_5.gif?ex=66ae6f73&is=66ad1df3&hm=d71e5271b29cadc625f9f01a7adaa1ac1d06db42533883a3c9e69ded3c6b1989&"
                 },
                 ["footer"] = {
                     ["text"] = "BONK HUB | " .. os.date("%H:%M:%S"),
